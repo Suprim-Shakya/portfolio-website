44a93af29e5591baa348d174e919f02678f3d01f
Suprim-Shakya <official_suprimshakya@outlook.com>
2024-03-17 20:56:31 +0545

    portfolio initial project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello! I am currently developing this website.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
